bc2e9ee3858cd98fe4f42679abbe533bf6fde2e9
vvgonline <vvgonline.net@gmail.com>
2025-12-01 16:14:56 +0530

    Add initial Visual Studio solution file for VVG.online.WEB.Test.PWA project

--- VVG.online.WEB.Test.PWA/Program.cs
+++ VVG.online.WEB.Test.PWA/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using VVG.online.WEB.Test.PWA;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
